648711ba72628b96097c28cbcd753ad0e216d7b2
Praveen Shinde <shindepraveen002@gmail.com>
2023-03-16 11:43:13 +0530

    Add cloudinary and saving the image init

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const puppeteer = require("puppeteer");
 require("dotenv").config();
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
@@ -17,7 +23,17 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/screenshot", async (req, res) => {
+app.post("/screenshot", (req, res) => {
+  console.log(req.body.url);
+  takeScreenshot(req.body.url)
+    .then((screenshot) => uploadScreenshot(screenshot))
+    .then((result) => {
+      console.log(result);
+      res.json({ result: result });
+    });
+});
+
+async function takeScreenshot(url) {
   // Create a browser instance
   const browser = await puppeteer.launch({
     args: [
@@ -38,26 +54,37 @@ app.post("/screenshot", async (req, res) => {
   // Set viewport width and height
   await page.setViewport({ width: 1920, height: 1080 });
 
-  const website_url = req.body.url;
+  const website_url = url;
 
-  console.log(req.body.url);
+  console.log(url);
 
   // Open URL in current page
   await page.goto(website_url, { waitUntil: "networkidle0" });
 
   // Capture screenshot
-  await page.screenshot({
-    path: (await page.title()) + ".jpg",
+  const screenshot = await page.screenshot({
+    omitBackground: false,
+    encoding: "binary",
   });
 
   // Close the browser instance
   await browser.close();
 
-  res.json({
-    success: true,
-  });
-});
+  return screenshot;
+}
 
 app.listen(port, () => {
   console.log("Listening on port " + port);
 });
+
+function uploadScreenshot(screenshot) {
+  return new Promise((resolve, reject) => {
+    const uploadOptions = {};
+    cloudinary.uploader
+      .upload_stream(uploadOptions, (error, result) => {
+        if (error) reject(error);
+        else resolve(result);
+      })
+      .end(screenshot);
+  });
+}
